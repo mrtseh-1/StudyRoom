3324a4656211d8b6774c08937a1b3749b7cc73ad
mrtseh-1 <mrtseh-1@student.ltu.se>
2022-04-05 13:43:55 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("GitHub är bra att ha");
+        System.out.println("Dags att skapa en ny kalkylator");
+    }
+}
